d5461840023d460c14b493aba6419773a481083e
Aram Anderson <aram.j.anderson@gmail.com>
2017-07-05 23:42:29 -0600

    all tested. sort, health, and height working

--- tests/tree_test.rb
+++ tests/tree_test.rb
@@ -68,7 +68,26 @@ class TreeTest < Minitest::Test
     expected = [{"I Love You Phillip Morris"=>7}, {"A Ballerina's Tale"=>9}, {"Love"=>11}, {"Meet My Valentine"=>17}, {"Bill & Ted's Bogus Journey"=>36}, {"Charlie's Country"=>38}, {"Crossroads"=>52}, {"Experimenter"=>55}, {"Armageddon"=>58}, {"The Land Before Time XIV: Journey of the Brave"=>65}, {"Hellions"=>67}, {"Collateral Damage"=>69}, {"Hannibal Buress: Animal Furnace"=>71}, {"French Dirty"=>75}, {"A Faster Horse"=>76}, {"A Picture of You"=>79}, {"Hannibal Buress: Comedy Camisado"=>80}, {"Charlie's Angels"=>86}, {"Bill & Ted's Excellent Adventure"=>93}, {"Animals United"=>98}]
 
     assert_equal expected, tree_1.sort
-    binding.pry
  end
+
+  def test_it_can_check_health_at_specific_depth
+    tree_1 = BinarySearchTree.new
+    tree_1.load('./tests/movies.txt')
+    tree_1.sort
+    expected = [[91, 2, 2], [88, 2, 2], [82, 2, 2], [61, 5, 5], [56, 1, 1], [54, 1, 1], [51, 9, 9], [40, 3, 3], [30, 2, 2], [28, 1, 1], [24, 7, 7], [3, 3, 3]]
+
+    assert_equal [[71, 99, 100]], tree_1.health(0)
+    assert_equal expected, tree_1.health(7)
+  end
+
+  def test_it_can_find_the_greatest_depth_in_the_tree
+    tree_1 = BinarySearchTree.new
+    tree_1.load('./tests/movies2.txt')
+    tree_2 = BinarySearchTree.new
+    tree_2.load('./tests/movies.txt')
+
+    assert_equal 5, tree_1.height
+    assert_equal 13, tree_2.height
+  end
 
 end
